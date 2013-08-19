9ae8a1a750a11f8fefc549da7008e9e4d97ac775
kutu62 <kutu62@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-19 20:56:02 +0000

    update and fix errors
    git-svn-id: https://plugins.svn.wordpress.org/sports-rankings-lists/trunk@758734 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-ranking.php
+++ wp-ranking.php
@@ -3,18 +3,13 @@
 Plugin Name: Wordpress List Ranking Plugin
 Plugin URI: http://wordpress.org
 Description: For a version without credits email kurt@fantasyknuckleheads.com - Credits only show if you enable advanced features - Useful for rankings and list of anything you desire but optimized for ranking athletes and sports teams. 
-Version: 1.6
+Version: 1.7
 Author: kutu62
 Author URI: http://fantasyknuckleheads.com
 */
-
-
-
 if ( ! function_exists('wp_ranking_post_types') ) {
-
 // Register Custom Post Types
 function wp_ranking_post_types() {
-
 	// Register Lists
 	$labels = array(
 		'name'                => _x( 'Player lists', 'Post Type General Name', 'wp-ranking' ),
@@ -31,7 +26,6 @@ function wp_ranking_post_types() {
 		'not_found'           => __( 'No lists found', 'wp-ranking' ),
 		'not_found_in_trash'  => __( 'No lists found in Trash', 'wp-ranking' ),
 	);
-
 	$args = array(
 		'label'               => __( 'player_list', 'wp-ranking' ),
 		'description'         => __( 'Player list', 'wp-ranking' ),
@@ -52,9 +46,7 @@ function wp_ranking_post_types() {
 		'publicly_queryable'  => true,
 		'capability_type'     => 'page',
 	);
-
 	register_post_type( 'player_list', $args );
-
 	// Register Rankers
 	$labels = array(
 			'name'                => _x( 'Rankers', 'Post Type General Name', 'wp-ranking' ),
@@ -71,7 +63,6 @@ function wp_ranking_post_types() {
 			'not_found'           => __( 'No rankers found', 'wp-ranking' ),
 			'not_found_in_trash'  => __( 'No rankers found in Trash', 'wp-ranking' ),
 		);
-
 		$args = array(
 			'label'               => __( 'ranker', 'wp-ranking' ),
 			'description'         => __( 'Rankers', 'wp-ranking' ),
@@ -91,19 +82,12 @@ function wp_ranking_post_types() {
 			'publicly_queryable'  => false,
 			'capability_type'     => 'page',
 		);
-
 		register_post_type( 'ranker', $args );
-
-
 }
-
 // Hook into the 'init' action
 add_action( 'init', 'wp_ranking_post_types', 0 );
-
 }
-
 if ( ! function_exists('list_category') ) {
-
 // Register List Categoties
 function list_category()  {
 	$labels = array(
@@ -122,7 +106,6 @@ function list_category()  {
 		'add_or_remove_items'        => __( 'Add or remove list categories', 'wp-ranking' ),
 		'choose_from_most_used'      => __( 'Choose from the most used list categories', 'wp-ranking' ),
 	);
-
 	$args = array(
 		'labels'                     => $labels,
 		'hierarchical'               => true,
@@ -132,45 +115,33 @@ function list_category()  {
 		'show_in_nav_menus'          => false,
 		'show_tagcloud'              => false,
 	);
-
 	register_taxonomy( 'list_category', 'player_list', $args );
-
-
 }
-
 register_taxonomy_for_object_type( 'list_category', 'player_list' );
-
 // Hook into the 'init' action
 add_action( 'init', 'list_category', 0 );
-
 }
-
 // Adding a shortocde [ranker id="id"]
 function ranker_shortcode( $atts ) {
-
 	$id = $atts['id']; // Get ranker id
 	$rankings = get_post_meta($id, '_rankings'); // Get rankings
 	$rankings = $rankings[0]; 
 	$list_id = get_post_meta($id, '_list', true); // Get list id
 	$players = get_post_meta($list_id, '_players'); // Get players from list
 	$players = $players[0];
-
 	ob_start();
 	?>
 	<style type="text/css">
 		.ranked-user {
 			padding: 5px;
 		}
-
 	</style>
 	<?php
 	global $rankers_counter;
 	if (!isset($rankers_counter)) {
 		$rankers_counter = 0;
 	} 
-
 	$rankers_counter++;
-	
 	echo '<table id="ranker' . $rankers_counter . '">';
 	echo '<thead>';
 	echo '<tr>';
@@ -185,7 +156,7 @@ function ranker_shortcode( $atts ) {
 		echo '<span class="ranked-user">' . $user_info->display_name . '</span>';
 		echo '</th>';		
 	}
-	if ($authors_count > 1) echo '<th class="author" onclick="sortTable(' . ($authors_count + 1) . ', \'ranker' . $rankers_counter . '\')" title="' . __( 'Click to sort by composite ranking', 'wp-ranking' ) . '">' . __( 'Composite', 'wp-ranking' ) . '</th>';
+if ($authors_count > 1) echo '<th class="author" onclick="sortTable(' . ($authors_count + 1) . ', \'ranker' . $rankers_counter . '\')" title="' . __( 'Click to sort by composite ranking', 'wp-ranking' ) . '">' . __( 'Composite', 'wp-ranking' ) . '</th>';
 	echo '</tr>';
 	echo '</thead>';
 	echo '<tbody>';
@@ -194,7 +165,6 @@ function ranker_shortcode( $atts ) {
 		if ($player_counter % 2 == 0) $class = 'even';
 		else $class = 'odd';
 		$player_counter++;
-
 		echo '<tr class="' . $class . '" id="player-' . $player['id'] . '">';
 		echo '<td class="player">';
 		echo $player['name'];
@@ -212,9 +182,7 @@ function ranker_shortcode( $atts ) {
 					$i++;					
 				}
 			}
-
 			echo '</td>';
-
 		}
 		if ($authors_count > 1) {
 			echo '<td class="sort">';
@@ -234,19 +202,14 @@ function ranker_shortcode( $atts ) {
 				echo $data['comment'];
 				echo '</div>';
 			}
-			
-
 		}
 	}
 	if (get_option( 'show-comments' ) || get_option( 'show-avatars' ) || get_option( 'allow-authors' )) {
 		echo '<br><p style="font-size:85%;">Want rankings like this? <a href="http://wordpress.org/plugins/sports-rankings-lists/">Download Plugin</a> or read more @ <a href="http://fantasyknuckleheads.com/" title="Fantasy Football for all you Knuckleheads">Fantasy Knuckleheads</a></p>';
 	}
-	
-
 	// Table sorting function
 	?>
 	<script>
-
 	    var authorsCount = <?php echo $authors_count; ?>;
 	    	if (authorsCount == 1) {
 	    		sortTable(1, 'ranker<?php echo $rankers_counter; ?>');
@@ -254,8 +217,6 @@ function ranker_shortcode( $atts ) {
 	    	else {
 	    		sortTable(authorsCount + 1, 'ranker<?php echo $rankers_counter; ?>');
 	    	}
-	
-	
 	</script>
 	<?php
 	$output = ob_get_contents();
@@ -263,23 +224,16 @@ function ranker_shortcode( $atts ) {
 	return $output ;
 }
 add_shortcode( 'ranker', 'ranker_shortcode' );
-
 function wp_ranking_styles() {
 if ( is_single() ) {
 	wp_enqueue_script('ranker-script', plugins_url( '/js/scripts.js' , __FILE__ ));
     wp_register_style( 'wp-ranking-style', plugins_url('/css/wp-ranking.css', __FILE__) );
     wp_enqueue_style( 'wp-ranking-style' );
 }}
- 
-
 add_action('wp_enqueue_scripts', 'wp_ranking_styles');
-
 include( plugin_dir_path( __FILE__ ) . 'admin.php');
-
 if (function_exists('load_plugin_textdomain'))
 	{
 		load_plugin_textdomain('wp-ranking', false, dirname(plugin_basename(__FILE__)) . '/languages/');
 	}
-
 ?>
-
